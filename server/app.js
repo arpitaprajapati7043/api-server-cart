5f32e61fa1db7b9c42142bcc434b77107c9d5526
arpita <arpitap433@gmail.com>
2023-12-28 09:00:12 -0800

    jk

--- server/app.js
+++ server/app.js
@@ -1,14 +1,27 @@
 const express = require('express');
+const helmet = require('helmet');
 const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+// Enable helmet with contentSecurityPolicy middleware
+app.use(helmet.contentSecurityPolicy({
+  useDefaults: true,
+  directives: {
+    defaultSrc: ["'self'"],
+    scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'", 'https://apis.google.com'],
+    // Add other directives as needed
+  },
+}));
+
 
 // Specify allowed origins using environment variable
 const allowedOrigins = process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',') : ['http://localhost:3000'];
 app.use(cors({ origin: allowedOrigins }));
 
 // Your routes and other middleware...
+
+
 app.get("/api/message", (req, res) => {
   res.json({ message: "Hello from the backend!" });
 });
